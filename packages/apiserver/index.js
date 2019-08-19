dbddfeda7f991a6908cde9f8da87f3edc53e1e47
Anthony Cerbic <kuzzdra@gmail.com>
2019-08-19 06:20:27 +0300

    nohoist all packages

--- packages/apiserver/index.js
+++ packages/apiserver/index.js
@@ -1,27 +1,32 @@
-'use strict'
-const express = require('express')
+"use strict";
+const express = require("express");
+const pino = require("pino");
+const pinoHttp = require("pino-http");
 
 // Create the express app
-const app = express()
+const app = express();
+
+const logger = pino();
+app.use(pinoHttp({ logger }));
 
 // Routes and middleware
 // app.use(/* ... */)
 // app.get(/* ... */)
 
 // Error handlers
-app.use(function fourOhFourHandler (req, res) {
-  res.status(404).send()
-})
-app.use(function fiveHundredHandler (err, req, res, next) {
-  console.error(err)
-  res.status(500).send()
-})
+app.use(function fourOhFourHandler(req, res) {
+    res.status(404).send();
+});
+app.use(function fiveHundredHandler(err, req, res, next) {
+    console.error(err);
+    res.status(500).send();
+});
 
 // Start server
-app.listen(1234, function (err) {
-  if (err) {
-    return console.error(err)
-  }
+app.listen(1234, function(err) {
+    if (err) {
+        return console.error(err);
+    }
 
-  console.log('Started at http://localhost:1234')
-})
+    console.log("Started at http://localhost:1234");
+});
